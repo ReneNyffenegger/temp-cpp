e62c1eb825291c901440d4b59123f0449c8e7d62
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-23 14:34:58 +0200

    n/a

--- friend-forward/prog.cpp
+++ friend-forward/prog.cpp
@@ -1,10 +1,20 @@
-struct obj {
+//
+//       https://stackoverflow.com/questions/57164169/class-with-friend-rather-than-forward-declaration-which-compiler-is-correct
+//
+#include <iostream>
 
+struct obj {
   friend int f(int);
 
+  void m(int x) { std::cout << "f(" << x << ") = " << f(x) << std::endl; }
 };
 
 int main() {
+  obj o;
+  o.m(21);
+}
 
 
+int f(int x) {
+  return 2*x;
 }
